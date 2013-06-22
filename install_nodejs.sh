4942a5c85060f446f0a1d58c73737c195fa7dfea
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-06-22 06:53:59 -0400

    edit install_nodejs

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -6,7 +6,13 @@ echo "#########################################################################"
 mkdir -p ~/local
 mkdir -p ~/local/src
 
-curl -0 http://nodejs.org/dist/node-latest.tar.gz | tar xzf -C ~/local/src/node
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xzf -C=~/local/src/node
+
+curl -O http://nodejs.org/dist/node-latest.tar.gz
+tar xvzf node-latest.tar.gz -C ~/local/src/
+rm -rf ~/local/src/node
+mv ~/local/src/node-* ~/local/src/node
+rm -rf node-latest.tar.gz
 
 cd ~/local/src/node
 ./configure --prefix=~/local
